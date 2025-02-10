96fe59ebd30b377cbd2527b006af5609abfb1aaa
RutaBukaityte <ruta.bukaitite2@gmail.com>
2025-02-10 19:24:39 +0200

    math_operations.py

---
+++ Homework/Rūta/math_operations.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Addition: {num1} + {num2} = {num1 + num2}")
+print(f"Subtraction: {num1} - {num2} = {num1 - num2}")
+print(f"Multiplication: {num1} * {num2} = {num1 * num2}")
+print(f"Power: {num1} ** {num2} = {num1 ** num2}")
+if num2 != 0:
+    print(f"Division: {num1} / {num2} = {num1 / num2}")
+    print(f"Remainder: {num1} % {num2} = {num1 % num2}")
+else:
+      print("Division and remainder osperations are not possible")
